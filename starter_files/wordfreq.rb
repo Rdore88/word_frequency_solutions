a0f85937bea141e2c7788bed9636e6e3f68ce2bc
Robby Dore <rdore88@gmail.com>
2017-07-19 17:21:14 -0400

    Initial Commit

---
+++ starter_files/wordfreq.rb
@@ -0,0 +1,47 @@
+class Wordfreq
+  STOP_WORDS = ['a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from',
+    'has', 'he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to',
+    'were', 'will', 'with']
+
+    def initialize(filename)
+      contents = File.read(filename)
+      contents.downcase.gsub(/\W+/, '')
+
+      @arr = contents.split(" ").select do |el|
+        if !STOP_WORDS.index(el.downcase)
+          el
+        end
+      end
+    end
+
+  def frequency(word)
+    output = @arr.select do |el|
+      el.downcase == word.downcase
+    end
+    output.length
+  end
+
+  def frequencies
+  end
+
+  def top_words(number)
+  end
+
+  def print_report
+  end
+end
+
+if __FILE__ == $0
+  filename = ARGV[0]
+  if filename
+    full_filename = File.absolute_path(filename)
+    if File.exists?(full_filename)
+      wf = Wordfreq.new(full_filename)
+      wf.print_report
+    else
+      puts "#{filename} does not exist!"
+    end
+  else
+    puts "Please give a filename as an argument."
+  end
+end
